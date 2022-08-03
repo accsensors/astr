bddc43ce74fb733bf82a291d4a3582cf3e24da4d
gneymark <69157774+gneymark@users.noreply.github.com>
2022-08-03 17:34:20 -0600

    Update shiny_ast.R
    troubleshooting sample success flag for shinyAST

--- R/shiny_ast.R
+++ R/shiny_ast.R
@@ -322,7 +322,7 @@ shiny_flag = function(df_h) {
   df_h <- df_h %>%
     dplyr::mutate(SampleSuccess = dplyr::case_when(
                                             ProgrammedRuntime=="indefinite" ~ "PASS",
-                                            OverallDuration==ProgrammedRuntime ~ "PASS",
+                                            OverallDuration!=ProgrammedRuntime ~ "FAIL",
                                             ShutdownMode == 1  ~ "PASS",
                                             ShutdownMode == 3 ~ "PASS",
                                             TRUE ~ "END"
